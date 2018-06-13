cfdd38ca8c91d79c03a1ae5d26b0997cc805f5c1
doubleface <christophe@cozycloud.cc>
2018-06-13 19:03:16 +0200

    feat: deezer connector implementation

--- src/index.js
+++ src/index.js
@@ -1,122 +1,165 @@
 const {
   BaseKonnector,
   requestFactory,
-  signin,
+  log,
+  errors,
   scrape,
   saveBills,
-  log
+  hydrateAndFilter,
+  addData
 } = require('cozy-konnector-libs')
 const request = requestFactory({
-  // the debug mode shows all the details about http request and responses. Very usefull for
-  // debugging but very verbose. That is why it is commented out by default
   // debug: true,
-  // activates [cheerio](https://cheerio.js.org/) parsing on each page
-  cheerio: true,
-  // If cheerio is activated do not forget to deactivate json parsing (which is activated by
-  // default in cozy-konnector-libs
+  cheerio: false,
   json: false,
-  // this allows request-promise to keep cookies between requests
   jar: true
 })
+const cheerio = require('cheerio')
+const moment = require('moment')
 
-const baseUrl = 'http://books.toscrape.com'
+const baseUrl = 'https://www.deezer.com'
+const PLAYLIST_DOCTYPE = 'io.cozy.deezer.playlists'
 
 module.exports = new BaseKonnector(start)
 
-// The start function is run by the BaseKonnector instance only when it got all the account
-// information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
-// the account information come from ./konnector-dev-config.json file
 async function start(fields) {
   log('info', 'Authenticating ...')
   await authenticate(fields.login, fields.password)
   log('info', 'Successfully logged in')
-  // The BaseKonnector instance expects a Promise as return of the function
-  log('info', 'Fetching the list of documents')
-  const $ = await request(`${baseUrl}/index.html`)
-  // cheerio (https://cheerio.js.org/) uses the same api as jQuery (http://jquery.com/)
-  log('info', 'Parsing list of documents')
-  const documents = await parseDocuments($)
 
-  // here we use the saveBills function even if what we fetch are not bills, but this is the most
-  // common case in connectors
+  log('info', 'Parsing bills')
+  const $ = await fetchBillsPage()
+  const bills = parseBills($)
+
   log('info', 'Saving data to Cozy')
-  await saveBills(documents, fields.folderPath, {
-    // this is a bank identifier which will be used to link bills to bank operations. These
-    // identifiers should be at least a word found in the title of a bank operation related to this
-    // bill. It is not case sensitive.
-    identifiers: ['books']
+  await saveBills(bills, fields.folderPath, {
+    identifiers: ['deezer'],
+    contentType: 'application/pdf'
   })
+
+  const playlists = await fetchPlayLists()
+
+  const playlistsToSave = await hydrateAndFilter(playlists, PLAYLIST_DOCTYPE, {
+    keys: ['id']
+  })
+
+  await addData(playlistsToSave, PLAYLIST_DOCTYPE)
 }
 
-// this shows authentication using the [signin function](https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#module_signin)
-// even if this in another domain here, but it works as an example
-function authenticate(username, password) {
-  return signin({
-    url: `http://quotes.toscrape.com/login`,
-    formSelector: 'form',
-    formData: { username, password },
-    // the validate function will check if
-    validate: (statusCode, $) => {
-      // The login in toscrape.com always works excepted when no password is set
-      if ($(`a[href='/logout']`).length === 1) {
-        return true
-      } else {
-        // cozy-konnector-libs has its own logging function which format these logs with colors in
-        // standalone and dev mode and as JSON in production mode
-        log('error', $('.error').text())
-        return false
-      }
+async function authenticate(mail, password) {
+  const result = await request.post(`${baseUrl}/ajax/action.php`, {
+    form: {
+      type: 'login',
+      mail,
+      password
     }
   })
+
+  if (result.trim().indexOf('success') === -1) {
+    throw new Error(errors.LOGIN_FAILED)
+  }
 }
 
-// The goal of this function is to parse a html page wrapped by a cheerio instance
-// and return an array of js objects which will be saved to the cozy by saveBills (https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#savebills)
-function parseDocuments($) {
-  // you can find documentation about the scrape function here :
-  // https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#scrape
-  const docs = scrape(
+async function fetchBillsPage() {
+  const html = await request(`${baseUrl}/account/subscription`)
+  return cheerio.load(html)
+}
+
+function parseBills($) {
+  const bills = scrape(
     $,
     {
-      title: {
-        sel: 'h3 a',
-        attr: 'title'
-      },
+      title: '.payment-history-title',
       amount: {
-        sel: '.price_color',
+        sel: '.payment-history-price',
         parse: normalizePrice
       },
-      fileurl: {
-        sel: 'img',
-        attr: 'src',
-        parse: src => `${baseUrl}/${src}`
+      currency: {
+        sel: '.payment-history-price',
+        parse: price => price.slice(-1)
       },
-      filename: {
-        sel: 'h3 a',
-        attr: 'title',
-        parse: title => `${title}.jpg`
+      date: {
+        sel: '.payment-history-date',
+        parse: date => moment(date, 'DD/MM/YYYY').toDate()
+      },
+      fileurl: {
+        sel: 'a',
+        attr: 'href'
       }
     },
-    'article'
-  )
-  return docs.map(doc => ({
-    ...doc,
-    // the saveBills function needs a date field
-    // even if it is a little artificial here (these are not real bills)
-    date: new Date(),
-    currency: '€',
-    vendor: 'template',
-    metadata: {
-      // it can be interesting that we add the date of import. This is not mandatory but may be
-      // usefull for debugging or data migration
-      importDate: new Date(),
-      // document version, usefull for migration after change of document structure
-      version: 1
+    '.history-list > li:not(:nth-child(1))'
+  ).map(bill => {
+    return {
+      ...bill,
+      filename: `${moment(bill.date).format('YYYY-MM-DD')}_deezer.pdf`,
+      vendor: 'Deezer',
+      metadata: {
+        importDate: new Date(),
+        version: 1
+      }
     }
-  }))
+  })
+
+  return bills
 }
 
-// convert a price string to a float
 function normalizePrice(price) {
-  return parseFloat(price.replace('£', '').trim())
+  return parseFloat(
+    price
+      .replace('€', '')
+      .replace(',', '.')
+      .trim()
+  )
+}
+
+async function fetchPlayLists() {
+  // first fetch the api token
+  const userData = await request(
+    `${baseUrl}/ajax/gw-light.php?method=deezer.getUserData&input=3&api_version=1.0&api_token=`
+  )
+  const api_token = userData.results.checkForm
+
+  // then get the playlists ids from the user menu
+  const userMenu = await request(
+    `${baseUrl}/ajax/gw-light.php?method=deezer.userMenu&input=3&api_version=1.0&api_token=${api_token}`
+  )
+
+  log('info', 'Fetching playlists details')
+  const playlists = []
+  for (const playlist of userMenu.results.PLAYLISTS.data) {
+    log('info', playlist.TITLE)
+    const details = await fetchPlayListDetails(playlist, api_token)
+    if (details) playlists.push(details)
+  }
+
+  return playlists
+}
+
+async function fetchPlayListDetails(playlist, api_token) {
+  const content = await request.post(
+    `${baseUrl}/ajax/gw-light.php?method=deezer.pagePlaylist&input=3&api_version=1.0&api_token=${api_token}`,
+    {
+      json: true,
+      body: {
+        playlist_id: playlist.PLAYLIST_ID,
+        lang: 'en',
+        nb: 1000000,
+        start: 0,
+        tab: 0,
+        tags: true,
+        header: true
+      }
+    }
+  )
+
+  if (!content.results) {
+    log('warn', 'No songs')
+    return false
+  }
+
+  return {
+    id: playlist.PLAYLIST_ID,
+    ...content.results.DATA,
+    songs: content.results.SONGS.data
+  }
 }
